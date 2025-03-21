fffab13c3475cc683ff95bc7d0642fde6ed4540e
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-21 22:27:48 +0300

    8

--- Tip Calculator/Views/Choose Tip View/CustomTipView.swift
+++ Tip Calculator/Views/Choose Tip View/CustomTipView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct CustomTipView: View {
+    @Binding var selectedTip: Int
+    @Binding var tipPercentage: Int
+    
+    @FocusState private var focusState: Bool
+    
+    var isSelected: Bool {
+        selectedTip == tipPercentage
+    }
+    
+    var fgColor: Color {
+        isSelected ? .myDarkGreen : .white
+    }
+    var bgColor: Color {
+        isSelected ? .white : .myDarkGreen
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottom) {
+            TextField("Custum",value: $tipPercentage, format: .percent)
+                .focused($focusState)
+                .onChange(of: focusState) { oldValue, newValue in
+                    selectedTip = TipPercentage.custom.rawValue
+                }
+                .fixedSize()
+                .keyboardType(.numberPad)
+                .font(.headline.weight(.semibold))
+                .foregroundStyle(fgColor)
+                .padding()
+                .background(bgColor.opacity(0.7))
+                .clipShape(Capsule())
+                .shadow(color: fgColor.opacity(0.3),radius: 3, x: 2, y: 2)
+            Text("Custom")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+        }
     }
 }
 
 #Preview {
-    CustomTipView()
+    CustomTipView(selectedTip: .constant(5), tipPercentage: .constant(TipPercentage.twenty.rawValue))
 }
