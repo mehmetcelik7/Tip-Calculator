ec73d461eb84712be44893ba8ba1a320d1b86abe
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-21 17:52:15 +0300

    Initial Commit

---
+++ Tip Calculator/Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorApp.swift
+//  Tip Calculator
+//
+//  Created by mehmet Çelik on 21.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
